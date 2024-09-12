629dcb7a5fec9eb00c20053c0ba97979b960ba0c
JairGarcia29 <jamesjair29@gmail.com>
2024-09-12 08:18:18 -0500

    Primer Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 0;
+        while (i <= 100)
+        {
+            
+            Console.WriteLine(i);
+            i++;
+        }
+        
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Realizar un programa que permita registrar una contraseña y luego validar  si es correcto(solo tiene 3 intentos), si es incorrecto cerrar el programa.//
+        int intentos=0;
+        string contra = "";
+        string Contraseña;
+        while (intentos <= 3)
+        {
+            Console.WriteLine("Ingrese la contraseña");
+            contra = Console.ReadLine();
+            if(contra == "Contraseña") {
+                Console.WriteLine("Bienvenido a tu perfil");
+                Console.WriteLine("Haz ingresado.....");
+                intentos = 4;
+            }
+            else
+            {
+                Console.WriteLine("ERRORRRRRRRRRR....");
+                Console.WriteLine("Tu cuenta a sido bloqueada");                
+            }
+        }
+    }
+}
